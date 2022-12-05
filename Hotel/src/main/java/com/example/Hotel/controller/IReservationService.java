b08f713dadfbdc23cf563636ad1c3a0b8d11fe32
MN-Sandratra <mn.sandratra@gmail.com>
2022-12-05 20:18:31 +0300

    grpc conflict rebase

--- Hotel/src/main/java/com/example/Hotel/controller/IReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.Hotel.controller;
-
-import com.example.Hotel.exception.NotPartnerExceptions;
-import com.example.Hotel.exception.OffreNotFoundExceptions;
-import java.util.UUID;
-
-public interface IReservationService {
-    public String Reservation(int id, String login, String Password, UUID offre, String nom, String prenom, String cartecredit) throws NotPartnerExceptions, OffreNotFoundExceptions, OffreNotFoundExceptions, NotPartnerExceptions;
-}
--- Hotel/src/main/java/com/example/Hotel/repositories/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Hotel.repositories;
-
-import com.example.Hotel.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
